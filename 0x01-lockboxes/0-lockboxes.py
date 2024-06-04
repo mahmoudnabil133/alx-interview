a100cadfeb41cbc5474caecedc8d10d87a4a33a8
mahmoudnabil133 <hodanabil144@gmail.com>
2024-06-04 15:33:50 +0300

    changed error

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,20 +1,19 @@
 #!/usr/bin/python3
 "game to itrerate rooms with keys"
-visited = set()
-
 
 def canUnlockAll(boxes):
     "check looked or unlocked"
-    dfs(boxes, 0)
+    visited = set()
+    dfs(boxes, 0, visited)
     for i in range(len(boxes)):
         if i not in visited:
             return False
     return True
 
 
-def dfs(rooms, i):
+def dfs(rooms, i, visited):
     "recursion to each room"
     visited.add(i)
     for k in rooms[i]:
         if k not in visited:
-            dfs(rooms, k)
+            dfs(rooms, k, visited)
