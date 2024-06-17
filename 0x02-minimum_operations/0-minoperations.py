219ff3bad56c4fc0efe77c5555edf79a391e1496
mahmoudnabil133 <hodanabil144@gmail.com>
2024-06-18 02:35:17 +0300

    min_operations

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+calculate minnnimum operations
+"""
+
+
+def minOperations(n):
+    "min operations"
+    if n <= 1:
+        return 0
+    op = 0
+    div = 2
+
+    while n > 1:
+        while n % div == 0:
+            op += div
+            n /= div
+        div += 1
+    return op
