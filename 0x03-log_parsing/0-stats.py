868f22a54d910e4b3e8ec713b23841fc845cd2ff
mahmoudnabil133 <hodanabil144@gmail.com>
2024-06-19 15:47:24 +0300

    check_log_parse

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -2,15 +2,18 @@
 "log parse module"
 import sys
 
+codes = ['200', '301', '400', '401', '403', '404', '405', '500']
+file_size = 0
+status = {}
+count = 0
+
 
 def log_parse(file_size, status):
     "log parse"
     print(f"File size: {file_size}")
     for k in sorted(status.keys()):
-        print(f"{k}: {status[k]}")
-file_size = 0
-status = {}
-count = 0
+        if k in codes:
+            print(f"{k}: {status[k]}")
 
 try:
     for l in sys.stdin:
