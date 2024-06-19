77f9b0ca52ba1b835ee37f36f6b0f33720a85ec3
mahmoudnabil133 <hodanabil144@gmail.com>
2024-06-19 15:49:35 +0300

    try

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -20,19 +20,19 @@ try:
         try:
             count += 1
             l = l.split(' ')
-            try:
-                file_size += int(l[-1][:-1])
-            except BaseException:
-                pass
-            try:
-                status[l[-2]] = status.get(l[-2], 0) + 1
-            except BaseException:
-                pass
+            # try:
+            file_size += int(l[-1][:-1])
+            # except BaseException:
+                # pass
+            # try:
+            status[l[-2]] = status.get(l[-2], 0) + 1
+            # except BaseException:
+            #     pass
             if count % 10 == 0:
                 log_parse(file_size, status)
-        except BaseException:
-            log_parse(file_size, status)
-            pass
+        # except BaseException:
+            # log_parse(file_size, status)
+            # pass
     log_parse(file_size, status)
 except KeyboardInterrupt:
     log_parse(file_size, status)
