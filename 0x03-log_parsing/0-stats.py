25d430ed09de34843ab067860f9d0729967f67e0
mahmoudnabil133 <hodanabil144@gmail.com>
2024-06-19 15:38:16 +0300

    log_parse

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -5,9 +5,9 @@ import sys
 
 def log_parse(file_size, status):
     "log parse"
-    print(f"File size: {file_size}")
-    for k, v in status.items():
-        print(f"{k}: {v}")
+    print(f"File size: {file_size: d}")
+    for k in sorted(status.keys()):
+        print(f"{k}: {status[k]}")
 file_size = 0
 status = {}
 count = 0
