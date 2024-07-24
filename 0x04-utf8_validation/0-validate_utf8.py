8c7eae44f2169f05649d8412bd96b3a2dd2843f4
mahmoudnabil133 <hodanabil144@gmail.com>
2024-07-24 20:56:31 +0300

    validate_utf-8

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -2,6 +2,18 @@
 """
 check utf-8 vallidation of data
 return True if valid otherwise false
+utf-8 rules
+
+for:
+
+1-byte:
+    0xxxxxxx
+2-byte:
+    110xxxxx 10xxxxxx
+3-byte:
+    1110xxxx 10xxxxxx 10xxxxxx
+4-byte:
+    11110xxx 10xxxxxx 10xxxxxx 10xxxxxx
 """
 
 
@@ -10,8 +22,34 @@ def validUTF8(data):
     check if data is
     valid or not
     """
+    "check validation of the bytes after first byte in the character"
 
-    for n in data:
-        if n < 0 and n > 256:
-            return False
-    return True
+    msb1 = 1 << 7
+    msb2 = 1 << 6
+    num_of_bytes = 0
+    for byte in data:
+        msb = 1 << 7
+
+        "started a new character"
+        if num_of_bytes == 0:
+            """
+            determine number of bytes
+            this character should be from
+            the first byte
+            """
+            while msb & byte:
+                num_of_bytes += 1
+                msb >>= 1
+            "if number of bytes is 0 then character is single byte"
+            if num_of_bytes == 0:
+                continue
+            "character should less than 4 bytes"
+            if num_of_bytes == 1 or num_of_bytes > 4:
+                return False
+        else:
+            "byte should be 10xxxxxx"
+            if not (msb1 & byte and not msb2 & byte):
+                return False
+        num_of_bytes -= 1
+    
+    return num_of_bytes == 0
