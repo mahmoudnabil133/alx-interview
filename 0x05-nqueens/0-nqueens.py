470ef7e23163b94138c582b4ec6228c3df4f97e5
mahmoudnabil133 <hodanabil144@gmail.com>
2024-07-30 03:39:45 +0300

    nqueens

---
+++ 0x05-nqueens/0-nqueens.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+"nqueens problem"
+import sys
+
+if len(sys.argv) >2:
+    print('Usage: nqueens N')
+    sys.exit(1)
+
+if not sys.argv[1].isdigit():
+    print('N must be a number')
+    sys.exit(1)
+
+n = int(sys.argv[1])
+if n < 4:
+    print('N must be at least 4')
+    sys.exit(1)
+
+cols = set()
+posDiag = set()
+negDiag = set()
+
+res = []
+board = []
+
+def backtrack(r):
+    if r == n:
+        res.append(board.copy())
+        return
+    
+    for c in range(n):
+        if c in cols or r-c in negDiag or r+c in posDiag:
+            continue
+        cols.add(c)
+        negDiag.add(r-c)
+        posDiag.add(r+c)
+        board.append([r, c])
+
+        backtrack(r+1)
+        cols.remove(c)
+        negDiag.remove(r-c)
+        posDiag.remove(r+c)
+        board.remove([r, c])
+backtrack(0)
+
+for result in res:
+    print(result)
