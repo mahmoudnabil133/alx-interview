848e157e8cc9725bc4c700775e3d346b4c0210c1
mahmoudnabil133 <hodanabil144@gmail.com>
2024-08-07 14:54:38 +0300

    semistandeed

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -2,27 +2,27 @@
 // request module for making http requests
 const request = require('request');
 
-request(`https://swapi-api.alx-tools.com/api/films/${process.argv[2]}`, async (error, response, body)=>{
-    if (error) {
-        console.log(error);
-    } else if (response.statusCode === 200) {
-        const film = JSON.parse(body)
-        const stars = film.characters
-        for (starUrl of stars) {
-            await new Promise((resolve, reject)=>{
-                request(starUrl, (error, response, body)=>{
-                    if (error) {
-                        reject(error)
-                    } else if (response.statusCode === 200) {
-                        const charcterData = JSON.parse(body)
-                        const charName = charcterData.name
-                        console.log(charName)
-                        resolve()
-                    }
-                })
-            })
-        }
-    } else {
-        console.log('failed to feach data, statusCode:', response.statusCode)
+request(`https://swapi-api.alx-tools.com/api/films/${process.argv[2]}`, async (error, response, body) => {
+  if (error) {
+    console.log(error);
+  } else if (response.statusCode === 200) {
+    const film = JSON.parse(body);
+    const stars = film.characters;
+    for (const starUrl of stars) {
+      await new Promise((resolve, reject) => {
+        request(starUrl, (error, response, body) => {
+          if (error) {
+            reject(error);
+          } else if (response.statusCode === 200) {
+            const charcterData = JSON.parse(body);
+            const charName = charcterData.name;
+            console.log(charName);
+            resolve();
+          }
+        });
+      });
     }
-})
+  } else {
+    console.log('Failed to fetch data, statusCode:', response.statusCode);
+  }
+});
